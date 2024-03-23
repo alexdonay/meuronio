85e2f6b80ad5abae02bb32dd6de51b62719bfa4d
Alexsander <alexdonay@gmail.com>
2024-03-23 16:40:54 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando um neurônio com uma taxa de aprendizado de 0.5
+        Neuronio neuronio = new Neuronio(0.5);
+
+        // Conjunto de entradas e saídas desejadas para a operação AND
+        double[][] entradas = new double[][]
+        {
+            new double[] {0, 0},
+            new double[] {0, 1},
+            new double[] {1, 0},
+            new double[] {1, 1}
+        };
+
+        double[] saidasDesejadas = new double[] { 0, 0, 0, 1 }; // Saídas desejadas para a operação AND
+
+        // Treinamento do neurônio
+        for (int epoch = 0; epoch < 100; epoch++)
+        {
+            for (int i = 0; i < entradas.Length; i++)
+            {
+                double saidaAtual = neuronio.Train(entradas[i], saidasDesejadas[i]);
+            }
+        }
+
+        // Após o treinamento, testando o neurônio com as entradas originais
+        Console.WriteLine("Testando o neurônio treinado para a operação AND:");
+        foreach (var entrada in entradas)
+        {
+            double output = Math.Round(neuronio.Predict(entrada));
+            Console.WriteLine($"Entrada: [{entrada[0]}, {entrada[1]}] - Saída: {output}");
+        }
+    }
+}
---
+++ neuronio.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+public class Neuronio
+{
+    private static readonly Random Random = new Random();
+
+    public double[] Weights { get; private set; }
+    public double Bias { get; private set; }
+    public double LearningRate { get; }
+
+    public Neuronio(double learningRate = 0.5)
+    {
+        Weights = Enumerable.Range(0, 2).Select(i => Random.NextDouble() * 2 - 1).ToArray();
+        Bias = Random.NextDouble() * 2 - 1; 
+        LearningRate = learningRate;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private double Sigmoid(double x)
+    {
+        return 1.0 / (1.0 + Math.Exp(-x));
+    }
+
+    public double Predict(double[] inputs)
+    {
+        if (inputs.Length != 2)
+        {
+            throw new ArgumentException("Input array must have length 2.");
+        }
+
+        double weightedSum = 0;
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            weightedSum += Weights[i] * inputs[i];
+        }
+
+        return Sigmoid(weightedSum + Bias);
+    }
+
+    public double Train(double[] inputs, double targetOutput)
+    {
+        if (inputs.Length != 2)
+        {
+            throw new ArgumentException("Input array must have length 2.");
+        }
+
+        double output = Predict(inputs);
+        double error = targetOutput - output;
+        for (int i = 0; i < Weights.Length; i++)
+        {
+            Weights[i] += LearningRate * error * inputs[i];
+        }
+
+        Bias += LearningRate * error;
+
+        return output;
+    }
+}
